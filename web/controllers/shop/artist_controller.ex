552c3b32de4ccc1ca5c4c7cbcca5024b8d4968ab
jekku <jekripreclaroorlina@gmail.com>
2017-05-03 18:51:26 +0800

    Cuts artist list into server side.

--- web/controllers/shop/artist_controller.ex
+++ web/controllers/shop/artist_controller.ex
@@ -1,8 +1,17 @@
 defmodule Inkink.Shop.ArtistController do
   use Inkink.Web, :controller
+  alias Inkink.{
+    Artist,
+    Artwork,
+    Repo
+  }
 
   def index(conn, _params) do
-    render conn, "index.html"
+    artists =
+      Artist
+      |> Repo.all
+      |> Repo.preload(:artworks)
+    render conn, "index.html", artists: artists
   end
 
   def show(conn, _params) do
