dcdc61f3b600e5db6887495c8e8fe5d3ad50e84c
jekku <jekripreclaroorlina@gmail.com>
2017-05-05 00:42:44 +0800

    Woops. Wrong folder.

--- web/views/shop/page_view.ex
+++ web/views/shop/page_view.ex
@@ -5,7 +5,7 @@ defmodule Inkink.Shop.PageView do
     url = Inkink.Avatar.url({artist.avatar, artist}, :original)
     case Mix.env do
       :dev -> Inkink.Endpoint.static_path(String.replace(url, "priv/static", ""))
-      _ -> String.replace(url, "uploads", "inkink-production/priv/static", global: false)
+      _ -> String.replace(url, "uploads", "inkink-production/uploads", global: false)
     end
   end
 
