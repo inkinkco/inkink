8c37bd91f698fb0a40e8d18a955c78deddc4d2c4
jekku <jekripreclaroorlina@gmail.com>
2017-05-05 00:51:01 +0800

    Fix url for artwork.

--- web/views/shop/page_view.ex
+++ web/views/shop/page_view.ex
@@ -10,7 +10,7 @@ defmodule Inkink.Shop.PageView do
   end
 
   def image_url(artwork) do
-    url = Inkink.Avatar.url({artwork.image, artwork}, :original)
+    url = Inkink.Image.url({artwork.image, artwork}, :original)
     case Mix.env do
       :dev -> Inkink.Endpoint.static_path(String.replace(url, "priv/static", ""))
       _ -> String.replace(url, "uploads", "inkink-production/uploads", global: false)
