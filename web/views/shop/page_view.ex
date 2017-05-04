b213abf69ffe28ca03fb6602480bbc71965245f2
jekku <jekripreclaroorlina@gmail.com>
2017-05-04 13:45:23 +0800

    Fix breaking changes from package downgrades.

--- web/views/shop/page_view.ex
+++ web/views/shop/page_view.ex
@@ -3,11 +3,11 @@ defmodule Inkink.Shop.PageView do
 
   def avatar_url(artist) do
     Inkink.Avatar.url({artist.avatar, artist}, :original)
-    |> String.replace("/priv/static", "")
+    |> String.replace("priv/static", "")
   end
 
   def image_url(artwork) do
     Inkink.Image.url({artwork.image, artwork}, :original)
-    |> String.replace("/priv/static", "")
+    |> String.replace("priv/static", "")
   end
 end
