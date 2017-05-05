f6a9e0b9e34ecc9c4e795c0022f38e64596a6051
jekku <jekripreclaroorlina@gmail.com>
2017-05-05 13:35:32 +0800

    Cuts @jelvin 's page into server side eex code.

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -9,19 +9,22 @@ defmodule Inkink.SessionController do
   plug :put_layout, {Inkink.Shop.LayoutView, "app.html"}
 
   def new(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", changeset: User.changeset(%User{}))
   end
 
   def create(conn, %{"email" => email, "password" => password}) do
     case Auth.authenticate({email, password}) do
       {:ok, user} ->
         Authplug.put_current_user(user)
+        conn |> redirect(to: admin_artist_path(conn, :index))
       {:error, error} ->
         Authplug.put_current_user(nil)
+        render(conn, "index.html", changeset: User.changeset(%User{}))
     end
   end
 
   def delete(conn, _params) do
     Authplug.put_current_user(nil)
+    redirect(conn, to: page_path(conn, :index))
   end
 end
